504002a8bca02a67b256642d72d8e6acaff200d1
JHOSELINE AYDEE FLORES VERGARA <aydee84vergara@gmail.com>
2026-04-08 22:23:03 -0300

    variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,39 @@
+/**
+ * Estudo das variaveis 
+ * @autor jhoseline
+ * 
+ * */
+console.clear()
+console.log("estudo da variaveis")
+console.log("")
+console.log("variavel do tipo string")
+//a linha abaixo criar uma variavel
+let nome="jhoseline"
+console.log(nome)
+//a linha baixo mostra o tipo de variavel
+console.log(typeof(nome))
+console.log("")
+console.log("variavel do tipo number")
+let idade =57
+console.log(idade)
+console.log(typeof(idade))
+let peso = 60
+let altura =1.60
+console.log("variavel do tipo boolean")
+let vip = true
+console.log (vip)
+console.log(typeof(vip))
+
+console.log("")
+console.log("--------------------")
+console.log(   "ficha do aluno"  )
+console.log("--------------------")
+//a linha abaixo concatena (união) um texto com o 
+// conteudo da variavel
+console.log(`nome:${nome}`)
+console.log(`idade:${idade}`)
+console.log(`peso:${peso}`)
+console.log(`altura:${altura}`)
+console.log(`vip:${vip}`)
+console.log("--------------------")
+
