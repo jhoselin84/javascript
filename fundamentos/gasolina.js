8a548d5ec9a5d5e7de821d6fc21edf3a5d8428b7
JHOSELINE AYDEE FLORES VERGARA <aydee84vergara@gmail.com>
2026-04-15 22:30:28 -0300

    gasolina.js

---
+++ fundamentos/gasolina.js
@@ -0,0 +1,24 @@
+//** convertor de gasolina
+// @uthor*/
+
+//importaão de pacote
+const prompt= require(`prompt-sync`)()
+
+//variaveis 
+let etanol, gasolina,
+
+console.clear()
+console.log ("etanol x gasolina")
+
+//entrada 
+ etanol=Number(prompt("digite do litro do etanol:"))
+ gasolina=Number(prompt("digite do litro da gasolina:"))
+ 
+ //processamento + saida
+ if (etanol<0.7 * gasolina) {
+    
+ } else {
+    console.log()
+ }()
+
+
